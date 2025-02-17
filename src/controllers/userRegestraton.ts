6c8a20087901f336c63dd12d0e4bc68971b8e90a
nayeem-ahammed <nayeemahammed853@gmail.com>
2025-02-18 01:52:34 +0600

    login and register api partially done

--- src/controllers/userRegestraton.ts
+++ src/controllers/userRegestraton.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { UserCreateSchema } from "../schema";
 import prisma from "../prisma";
-
+import bcrypt from "bcryptjs";
+import axios from "axios";
+import { USER_SERVICE } from "../config";
 
 const userRegistration = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try{
@@ -16,6 +18,39 @@ const userRegistration = async (req: Request, res: Response, next: NextFunction)
                 email: parseBody.data.email,
             },
         });
+
+        if(existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        };
+
+        const salt = await bcrypt.genSaltSync(10);
+        const hashedPassword = await bcrypt.hash(parseBody.data.password, salt);
+
+
+        const user = await prisma.user.create({
+            data: {
+                ...parseBody.data,
+                password: hashedPassword,
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                role: true,
+                status: true,
+                createdAt: true,
+            }
+        });
+        console.log("User created successfully",user);
+
+        await axios.post(`${USER_SERVICE}users`, {
+            authUserId: user.id,
+            name: user.name,
+            email: user.email,
+        })
+        return res.status(201).json(user);
+
+
     }
     catch(err) {
         next(err);
--- src/schema.ts
+++ src/schema.ts
@@ -5,3 +5,8 @@ export const UserCreateSchema = z.object({
     password: z.string().min(6).max(100),
     name: z.string().max(255),
 })
+
+export const UserLoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+})
